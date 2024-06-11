231b2f8206aa1d4492daa8ba0b829e70726e3632
Jameson <jameson@Jamesons-MacBook-Pro.local>
2024-06-11 13:12:50 -0500

    Removing babel

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,9 @@
-// module.exports = {
-//     preset: 'ts-jest',
-//     collectCoverage: true,
-//     coverageDirectory: 'coverage',
-//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'css', 'json'],
-//     moduleNameMapper: {
-//         '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
-//     },
-//     moduleDirectories: ['node_modules', 'src'],
-//     transform: {
-//         '^.+\\.(css)$': 'jest-css-modules',
-//         '^.+\\.(ts|tsx)$': 'ts-jest',
-//     },
-// }
-
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.[t|j]sx?$': 'babel-jest',
+        '^.+\\.(css)$': 'jest-css-modules',
+        '^.+\\.(ts|tsx)$': 'ts-jest',
     },
     moduleNameMapper: {
         '\\.(css|less)$': 'identity-obj-proxy',
